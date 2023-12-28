6069d60ba64b22eb88239d021999e8e42c5070b5
Sergio Gonzalez <89554725+sergiogonzalezyes@users.noreply.github.com>
2023-12-28 16:55:40 -0600

    Fixed DB auto-increment for all tables.

--- InventoryMGMT-SYSTEM.NET/Data/InventoryMGMTDbContext.cs
+++ InventoryMGMT-SYSTEM.NET/Data/InventoryMGMTDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using InventoryMGMT_SYSTEM.NET.Models;
 
@@ -12,7 +14,7 @@ public partial class InventoryMGMTDbContext : DbContext
     }
 
     public InventoryMGMTDbContext(DbContextOptions<InventoryMGMTDbContext> options)
-        : base(options)
+            : base(options)
     {
     }
 
@@ -30,18 +32,18 @@ public partial class InventoryMGMTDbContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=InventoryMGMTConnectionString");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Item>(entity =>
         {
-            entity.HasKey(e => e.ItemId).HasName("PK__Item__3FB50F94BC6FF224");
+            entity.HasKey(e => e.ItemId).HasName("PK__Item__3FB50F949A8F0955");
 
             entity.ToTable("Item");
 
-            entity.Property(e => e.ItemId)
-                .ValueGeneratedNever()
-                .HasColumnName("Item_ID");
+            entity.Property(e => e.ItemId).HasColumnName("Item_ID");
             entity.Property(e => e.ItemCost)
                 .HasColumnType("decimal(19, 4)")
                 .HasColumnName("Item_Cost");
@@ -58,22 +60,20 @@ public partial class InventoryMGMTDbContext : DbContext
 
             entity.HasOne(d => d.ItemStatus).WithMany(p => p.Items)
                 .HasForeignKey(d => d.ItemStatusId)
-                .HasConstraintName("FK__Item__Item_Statu__6A30C649");
+                .HasConstraintName("FK__Item__Item_Statu__04E4BC85");
 
             entity.HasOne(d => d.ItemType).WithMany(p => p.Items)
                 .HasForeignKey(d => d.ItemTypeId)
-                .HasConstraintName("FK__Item__Item_Type___693CA210");
+                .HasConstraintName("FK__Item__Item_Type___03F0984C");
         });
 
         modelBuilder.Entity<ItemType>(entity =>
         {
-            entity.HasKey(e => e.ItemTypeId).HasName("PK__Item_Typ__335FBDF29EEEC07C");
+            entity.HasKey(e => e.ItemTypeId).HasName("PK__Item_Typ__335FBDF248E18D61");
 
             entity.ToTable("Item_Type");
 
-            entity.Property(e => e.ItemTypeId)
-                .ValueGeneratedNever()
-                .HasColumnName("Item_Type_ID");
+            entity.Property(e => e.ItemTypeId).HasColumnName("Item_Type_ID");
             entity.Property(e => e.ItemTypeName)
                 .HasMaxLength(50)
                 .IsUnicode(false)
@@ -82,13 +82,11 @@ public partial class InventoryMGMTDbContext : DbContext
 
         modelBuilder.Entity<Notification>(entity =>
         {
-            entity.HasKey(e => e.NotificationId).HasName("PK__Notifica__8C1160B59777CB0C");
+            entity.HasKey(e => e.NotificationId).HasName("PK__Notifica__8C1160B5CE770171");
 
             entity.ToTable("Notification");
 
-            entity.Property(e => e.NotificationId)
-                .ValueGeneratedNever()
-                .HasColumnName("Notification_ID");
+            entity.Property(e => e.NotificationId).HasColumnName("Notification_ID");
             entity.Property(e => e.NotificationDate)
                 .HasColumnType("datetime")
                 .HasColumnName("Notification_Date");
@@ -98,26 +96,24 @@ public partial class InventoryMGMTDbContext : DbContext
 
             entity.HasOne(d => d.NotificationType).WithMany(p => p.Notifications)
                 .HasForeignKey(d => d.NotificationTypeId)
-                .HasConstraintName("FK__Notificat__Notif__75A278F5");
+                .HasConstraintName("FK__Notificat__Notif__10566F31");
 
             entity.HasOne(d => d.RentalOrder).WithMany(p => p.Notifications)
                 .HasForeignKey(d => d.RentalOrderId)
-                .HasConstraintName("FK__Notificat__Renta__74AE54BC");
+                .HasConstraintName("FK__Notificat__Renta__0F624AF8");
 
             entity.HasOne(d => d.User).WithMany(p => p.Notifications)
                 .HasForeignKey(d => d.UserId)
-                .HasConstraintName("FK__Notificat__User___73BA3083");
+                .HasConstraintName("FK__Notificat__User___0E6E26BF");
         });
 
         modelBuilder.Entity<NotificationType>(entity =>
         {
-            entity.HasKey(e => e.NotificationTypeId).HasName("PK__Notifica__7732B5610E3AD4A4");
+            entity.HasKey(e => e.NotificationTypeId).HasName("PK__Notifica__7732B56108420CCE");
 
             entity.ToTable("Notification_Type");
 
-            entity.Property(e => e.NotificationTypeId)
-                .ValueGeneratedNever()
-                .HasColumnName("Notification_Type_ID");
+            entity.Property(e => e.NotificationTypeId).HasColumnName("Notification_Type_ID");
             entity.Property(e => e.NotificationTypeName)
                 .HasMaxLength(50)
                 .IsUnicode(false)
@@ -126,13 +122,11 @@ public partial class InventoryMGMTDbContext : DbContext
 
         modelBuilder.Entity<RentalOrder>(entity =>
         {
-            entity.HasKey(e => e.RentalOrderId).HasName("PK__Rental_O__AB32D1ABD08CE06B");
+            entity.HasKey(e => e.RentalOrderId).HasName("PK__Rental_O__AB32D1ABDCEAAD35");
 
             entity.ToTable("Rental_Order");
 
-            entity.Property(e => e.RentalOrderId)
-                .ValueGeneratedNever()
-                .HasColumnName("Rental_Order_ID");
+            entity.Property(e => e.RentalOrderId).HasColumnName("Rental_Order_ID");
             entity.Property(e => e.EndDate)
                 .HasColumnType("datetime")
                 .HasColumnName("End_Date");
@@ -146,7 +140,7 @@ public partial class InventoryMGMTDbContext : DbContext
 
             entity.HasOne(d => d.User).WithMany(p => p.RentalOrders)
                 .HasForeignKey(d => d.UserId)
-                .HasConstraintName("FK__Rental_Or__User___6D0D32F4");
+                .HasConstraintName("FK__Rental_Or__User___07C12930");
 
             entity.HasMany(d => d.Items).WithMany(p => p.RentalOrders)
                 .UsingEntity<Dictionary<string, object>>(
@@ -154,14 +148,14 @@ public partial class InventoryMGMTDbContext : DbContext
                     r => r.HasOne<Item>().WithMany()
                         .HasForeignKey("ItemId")
                         .OnDelete(DeleteBehavior.ClientSetNull)
-                        .HasConstraintName("FK__Rental_Or__Item___70DDC3D8"),
+                        .HasConstraintName("FK__Rental_Or__Item___0B91BA14"),
                     l => l.HasOne<RentalOrder>().WithMany()
                         .HasForeignKey("RentalOrderId")
                         .OnDelete(DeleteBehavior.ClientSetNull)
-                        .HasConstraintName("FK__Rental_Or__Renta__6FE99F9F"),
+                        .HasConstraintName("FK__Rental_Or__Renta__0A9D95DB"),
                     j =>
                     {
-                        j.HasKey("RentalOrderId", "ItemId").HasName("PK__Rental_O__F8C981528FF14E8D");
+                        j.HasKey("RentalOrderId", "ItemId").HasName("PK__Rental_O__F8C98152F0112091");
                         j.ToTable("Rental_Order_Item");
                         j.IndexerProperty<int>("RentalOrderId").HasColumnName("Rental_Order_ID");
                         j.IndexerProperty<int>("ItemId").HasColumnName("Item_ID");
@@ -170,13 +164,11 @@ public partial class InventoryMGMTDbContext : DbContext
 
         modelBuilder.Entity<Status>(entity =>
         {
-            entity.HasKey(e => e.StatusId).HasName("PK__Status__519009AC25EDBA0D");
+            entity.HasKey(e => e.StatusId).HasName("PK__Status__519009AC522F7CB6");
 
             entity.ToTable("Status");
 
-            entity.Property(e => e.StatusId)
-                .ValueGeneratedNever()
-                .HasColumnName("Status_ID");
+            entity.Property(e => e.StatusId).HasColumnName("Status_ID");
             entity.Property(e => e.StatusName)
                 .HasMaxLength(50)
                 .IsUnicode(false)
@@ -185,13 +177,11 @@ public partial class InventoryMGMTDbContext : DbContext
 
         modelBuilder.Entity<User>(entity =>
         {
-            entity.HasKey(e => e.UserId).HasName("PK__User__206D919053C2A2E1");
+            entity.HasKey(e => e.UserId).HasName("PK__User__206D9190B98B7B29");
 
             entity.ToTable("User");
 
-            entity.Property(e => e.UserId)
-                .ValueGeneratedNever()
-                .HasColumnName("User_ID");
+            entity.Property(e => e.UserId).HasColumnName("User_ID");
             entity.Property(e => e.Email)
                 .HasMaxLength(50)
                 .IsUnicode(false);
